f680dee45634030bc78273ad559da992f3ff1c8f
gtirimbo <32733214+gtirimbo@users.noreply.github.com>
2025-08-17 20:56:55 +0200

    Update FootballModel.py

--- FootballModel.py
+++ FootballModel.py
@@ -1,3 +1,21 @@
+def expected_goals(mu, mu_home, att_team, def_opponent):
+    """
+    Calculate expected goals using the log-linear Poisson model.
+
+    Parameters:
+    - mu (float): base log-average of away goals
+    - mu_home (float): home advantage (log difference)
+    - att_team (float): attack rating of the team
+    - def_opponent (float): defense rating of the opponent
+
+    Returns:
+    - float: expected number of goals
+    """
+    return np.exp(mu + mu_home + att_team + def_opponent)
+
+from scipy.stats import poisson
+
+
 class FootballModel:
     def __init__(self, params):
         self.params = params        # DataFrame with Team as index
